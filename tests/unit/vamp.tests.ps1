f49d5a77d72994034cf796a48c724f81f0bfd84e
Flynn Bundy <flynn.sbundy@gmail.com>
2016-07-02 21:02:20 +0200

    Update vamp.tests.ps1

--- tests/unit/vamp.tests.ps1
+++ tests/unit/vamp.tests.ps1
@@ -4,10 +4,10 @@ Import-Module .\PSYaml\PSYaml.psm1 -Verbose
 Describe 'Yaml Conversion' -Tags 'Unit' {
   Context 'PSYaml Module' {
     It 'Should be able to convert to Yaml example.yml' {
-       { ConvertFrom-Yaml -Path $PSScriptRoot\example.yml } | Should not throw
+       { ConvertFrom-Yaml -Path .\example.yml } | Should not throw
     }
     It 'Should be able to convert to Yaml vampspec.yml' {
-       { ConvertFrom-Yaml -Path $PSScriptRoot\vampspec.yml } | Should not throw
+       { ConvertFrom-Yaml -Path .\vampspec.yml } | Should not throw
     }
   }
 }
