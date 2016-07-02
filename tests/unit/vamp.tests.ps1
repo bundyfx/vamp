23595ae53e2b944b38f86741220b5e17c6a5da99
Flynn Bundy <flynn.sbundy@gmail.com>
2016-07-02 20:51:16 +0200

    added first test

--- tests/unit/vamp.tests.ps1
+++ tests/unit/vamp.tests.ps1
@@ -1,7 +1,13 @@
-Describe 'Im a test' {
-  Context 'Under this Context' {
-    It 'Should be true' {
-      $true | Should be $true
+Import-Module $PSScriptRoot\vamp.ps1 -Verbose
+Import-Module $PSScriptRoot\PSYaml\PSYaml.psm1 -Verbose
+
+Describe 'Yaml Conversion' -Tags 'Unit' {
+  Context 'PSYaml Module' {
+    It 'Should be able to convert to Yaml example.yml' {
+       { ConvertFrom-Yaml -Path $PSScriptRoot\example.yml } | Should not throw
+    }
+    It 'Should be able to convert to Yaml vampspec.yml' {
+       { ConvertFrom-Yaml -Path $PSScriptRoot\vampspec.yml } | Should not throw
     }
   }
 }
